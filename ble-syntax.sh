2f2f1fa9f0aac26decf60a10da762174311eda25
Koichi Murase <myoga.murase@gmail.com>
2015-12-03 10:34:46 +0900

    bugfix ble-syntax/parse/nest-equals parent_inest for negative onest[3].

--- ble-syntax.sh
+++ ble-syntax.sh
@@ -548,9 +548,9 @@ function ble-syntax/parse/nest-equals {
     local -a onest
     onest=($_onest)
 #%if !release
-    ((0<onest[3]&&onest[3]<=parent_inest)) || { ble-stackdump 'invalid nest' && return 0; }
+    ((onest[3]!=0&&onest[3]<=parent_inest)) || { ble-stackdump 'invalid nest' && return 0; }
 #%end
-    ((parent_inest-=onest[3]))
+    ((onest[3]<0?(parent_inest=onest[3]):(parent_inest-=onest[3])))
   done
 }
 
