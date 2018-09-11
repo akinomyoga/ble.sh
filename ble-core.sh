2f8f12095789acab0b76349f8b5783223ec49fb8
Koichi Murase <myoga.murase@gmail.com>
2018-09-11 20:22:09 +0900

    (cherry-pick) vi: fix usage of the builtin "unset"

--- ble-core.sh
+++ ble-core.sh
@@ -15,7 +15,7 @@ shopt -s checkwinsize
 #------------------------------------------------------------------------------
 # util
 
-function ble/util/unlocal { builtin unset "$1"; }
+function ble/util/unlocal { builtin unset "$@"; }
 
 _ble_util_read_stdout_tmp="$_ble_base_tmp/$$.read-stdout.tmp"
 # function ble/util/assign { builtin eval "$1=\"\$(${@:2})\""; }
