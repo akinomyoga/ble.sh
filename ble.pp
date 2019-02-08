akinomyoga/ble.sh

commit 77d459d747dac46748cadd70a8fc85ee3992bc6f

global: support "--help" of functions for users

ble.pp
@@ -480,9 +480,23 @@ if ! ble/base/initialize-cache-directory; then
   echo "ble.sh: failed to initialize \$_ble_base_cache." 1>&2
   return 1
 fi
-
+function ble/base/print-usage-for-no-argument-command {
+  local name=${FUNCNAME[1]} desc=$1; shift
+  printf '%s\n' \
+         "usage: $name" \
+         '' \
+         "$desc" \
+         '' >&2
+  [[ $1 != --help ]] && return 2
+  return 0
+}
 #%$ pwd=$(pwd) q=\' Q="'\''" bash -c 'echo "_ble_base_repository=$q${pwd//$q/$Q}$q"'
 function ble-update {
+  if (($#)); then
+    ble/base/print-usage-for-no-argument-command 'Update and reload ble.sh.' "$@"
+    return
+  fi
+
   if ! type git make gawk &>/dev/null; then
     local command
     for command in git make gawk; do
@@ -538,6 +552,11 @@ ble/bin/.freeze-utility-path gawk
 
 _ble_attached=
 function ble-attach {
+  if (($#)); then
+    ble/base/print-usage-for-no-argument-command 'Attach to ble.sh.' "$@"
+    return
+  fi
+
   [[ $_ble_attached ]] && return
   _ble_attached=1
 
@@ -581,6 +600,11 @@ function ble-attach {
 }
 
 function ble-detach {
+  if (($#)); then
+    ble/base/print-usage-for-no-argument-command 'Detach from ble.sh.' "$@"
+    return
+  fi
+
   [[ $_ble_attached ]] || return
   _ble_attached=
 

lib/core-complete.sh
@@ -3028,8 +3028,34 @@ fi
 ##   静的略語展開を登録します。
 function ble-sabbrev {
   if (($#)); then
+    local -a specs=()
+    local arg flag_help= flag_error=
+    for arg; do
+      if [[ $arg == *=* ]]; then
+        ble/array#push specs "$spec"
+      else
+        case $arg in
+        (--help) flag_help=1 ;;
+        (-*)
+          echo "ble-sabbrev: unknown option '$arg'." >&2
+          flag_error=1 ;;
+        (*)
+          echo "ble-sabbrev: unrecognized argument '$arg'." >&2
+          flag_error=1 ;;
+        esac
+      fi
+    done
+    if [[ $flag_help || $flag_error ]]; then
+      printf '%s\n' \
+             'usage: ble-sabbrev key=value' \
+             'usage: ble-sabbrev --help' \
+             '' \
+             'Register sabbrev expansion.'
+      [[ ! $flag_error ]]; return
+    fi
+
     local spec key value
-    for spec; do
+    for spec in "${specs[@]}"; do
       key=${spec%%=*} value=${spec#*=}
       ble-complete/sabbrev/register "$key" "$value"
     done

src/color.sh
@@ -25,6 +25,11 @@ if [[ ! ${bleopt_term_index_colors+set} ]]; then
 fi
 
 function ble-color-show {
+  if (($#)); then
+    ble/base/print-usage-for-no-argument-command 'Update and reload ble.sh.' "$@"
+    return
+  fi
+
   local cols=16
   local bg bg0 bgN ret gflags=$((_ble_color_gflags_BackColor|_ble_color_gflags_ForeColor))
   for ((bg0=0;bg0<256;bg0+=cols)); do
@@ -417,8 +422,28 @@ if [[ ! $_ble_faces_count ]]; then # reload #D0875
   _ble_faces=()
   _ble_faces_sgr=()
 fi
-function ble-color-defface   { local q=\' Q="'\''"; ble/array#push _ble_color_faces_defface_hook "ble-color-defface '${1//$q/$Q}' '${2//$q/$Q}'"; }
-function ble-color-setface   { local q=\' Q="'\''"; ble/array#push _ble_color_faces_setface_hook "ble-color-setface '${1//$q/$Q}' '${2//$q/$Q}'"; }
+
+function ble-color-defface/.check-argument {
+  local rex='^[a-zA-Z0-9_]+$'
+  [[ $1 =~ $rex && $2 ]] && return 0
+
+  local name=${FUNCNAME[1]}
+  printf '%s\n' "usage: $name FACE_NAME [TYPE:]SPEC" "" \
+         "TYPE" \
+         "  Specifies the format of SPEC. The following values are available." \
+         "" \
+         "  gspec   Comma separated graphic attribute list" \
+         "  g       Integer value" \
+         "  face    Face name" \
+         "  iface   Face id" \
+         "  sgrspec Parameters to the control function SGR" \
+         "  ansi    ANSI Sequences"
+  ext=2; [[ $1 == --help ]] && ext=0
+  return 1
+} >&2
+
+function ble-color-defface   { local ext; ble-color-defface/.check-argument "$@" || return "$ext"; local q=\' Q="'\''"; ble/array#push _ble_color_faces_defface_hook "ble-color-defface '${1//$q/$Q}' '${2//$q/$Q}'"; }
+function ble-color-setface   { local ext; ble-color-defface/.check-argument "$@" || return "$ext"; local q=\' Q="'\''"; ble/array#push _ble_color_faces_setface_hook "ble-color-setface '${1//$q/$Q}' '${2//$q/$Q}'"; }
 function ble-color-face2g    { ble-color/faces/initialize && ble-color-face2g    "$@"; }
 function ble-color-face2sgr  { ble-color/faces/initialize && ble-color-face2sgr  "$@"; }
 function ble-color-iface2g   { ble-color/faces/initialize && ble-color-iface2g   "$@"; }
@@ -458,6 +483,7 @@ function ble-color/faces/initialize {
   }
 
   function ble-color-defface {
+    local ext; ble-color-defface/.check-argument "$@" || return "$ext"
     local name=_ble_faces__$1 spec=$2 ret
     (($name)) && return
     (($name=++_ble_faces_count))
@@ -465,6 +491,7 @@ function ble-color/faces/initialize {
     ble-color-g2sgr "$ret"; _ble_faces_sgr[$name]=$ret
   }
   function ble-color-setface {
+    local ext; ble-color-defface/.check-argument "$@" || return "$ext"
     local name=_ble_faces__$1 spec=$2 ret
     if [[ ${!name} ]]; then
       ble-color-setface/.spec2g "$spec"; _ble_faces[$name]=$ret

commit f4d03f6e10868cbc0472417a3fc4e3ebe9a15fe7

complete: fix a bug of ble-sabbrev introduced by 77d459d

ble.pp
@@ -484,9 +484,7 @@ function ble/base/print-usage-for-no-argument-command {
   local name=${FUNCNAME[1]} desc=$1; shift
   printf '%s\n' \
          "usage: $name" \
-         '' \
-         "$desc" \
-         '' >&2
+         "$desc" >&2
   [[ $1 != --help ]] && return 2
   return 0
 }

lib/core-complete.sh
@@ -3031,8 +3031,8 @@ function ble-sabbrev {
     local -a specs=()
     local arg flag_help= flag_error=
     for arg; do
-      if [[ $arg == *=* ]]; then
-        ble/array#push specs "$spec"
+      if [[ $arg == ?*=* ]]; then
+        ble/array#push specs "$arg"
       else
         case $arg in
         (--help) flag_help=1 ;;
@@ -3049,7 +3049,6 @@ function ble-sabbrev {
       printf '%s\n' \
              'usage: ble-sabbrev key=value' \
              'usage: ble-sabbrev --help' \
-             '' \
              'Register sabbrev expansion.'
       [[ ! $flag_error ]]; return
     fi

src/color.sh
@@ -425,20 +425,20 @@ fi
 
 function ble-color-defface/.check-argument {
   local rex='^[a-zA-Z0-9_]+$'
-  [[ $1 =~ $rex && $2 ]] && return 0
+  [[ $# == 2 && $1 =~ $rex && $2 ]] && return 0
 
   local name=${FUNCNAME[1]}
-  printf '%s\n' "usage: $name FACE_NAME [TYPE:]SPEC" "" \
-         "TYPE" \
-         "  Specifies the format of SPEC. The following values are available." \
-         "" \
-         "  gspec   Comma separated graphic attribute list" \
-         "  g       Integer value" \
-         "  face    Face name" \
-         "  iface   Face id" \
-         "  sgrspec Parameters to the control function SGR" \
-         "  ansi    ANSI Sequences"
-  ext=2; [[ $1 == --help ]] && ext=0
+  printf '%s\n' "usage: $name FACE_NAME [TYPE:]SPEC" '' \
+         'TYPE' \
+         '  Specifies the format of SPEC. The following values are available.' \
+         '' \
+         '  gspec   Comma separated graphic attribute list' \
+         '  g       Integer value' \
+         '  face    Face name' \
+         '  iface   Face id' \
+         '  sgrspec Parameters to the control function SGR' \
+         '  ansi    ANSI Sequences'
+  ext=2; [[ $# == 1 && $1 == --help ]] && ext=0
   return 1
 } >&2
 