9b7939b8e7872aa6a5f22492ae4862ced639eba4
Koichi Murase <myoga.murase@gmail.com>
2019-05-04 09:20:41 +0900

    decode: add support for "S8C1T" key sequences

--- lib/init-cmap.sh
+++ lib/init-cmap.sh
@@ -42,10 +42,18 @@
 # 2019-04-15 __error__ を追加したので keycode の再生成が必要
 #
 
+function ble/init:cmap/bind-single-csi {
+  ble-bind -k "ESC [ $1" "$2"
+  ble-bind -k "CSI $1" "$2"
+}
+function ble/init:cmap/bind-single-ss3 {
+  ble-bind -k "ESC O $1" "$2"
+  ble-bind -k "SS3 $1" "$2"
+}
 function ble/init:cmap/bind-keypad-key {
   local Ft=$1 name=$2
   ble-bind --csi "$Ft" "$name"
-  (($3&1)) && ble-bind -k "ESC O $Ft" "$name"
+  (($3&1)) && ble/init:cmap/bind-single-ss3 "$Ft" "$name"
   (($3&2)) && ble-bind -k "ESC ? $Ft" "$name"
 }
 
@@ -148,7 +156,7 @@ function ble/init:cmap/initialize {
   #   Note: kp～ と通常のキーを区別しても binding が大変なだけで
   #   余り利点もないので取り敢えずこの設定では区別しない。
   ble-bind -k 'ESC ? SP' SP # kpspace
-  ble-bind -k 'ESC O SP' SP # kpspace
+  ble/init:cmap/bind-single-ss3 'SP' SP # kpspace
   ble/init:cmap/bind-keypad-key 'A' up    1
   ble/init:cmap/bind-keypad-key 'B' down  1
   ble/init:cmap/bind-keypad-key 'C' right 1
@@ -182,38 +190,38 @@ function ble/init:cmap/initialize {
 
   # rxvt
   #   Note: "CSI code @", "CSI code ^" は本体側で特別に処理している。
-  ble-bind -k 'ESC [ Z'     S-TAB
-  ble-bind -k 'ESC O a'     C-up
-  ble-bind -k 'ESC [ a'     S-up
-  ble-bind -k 'ESC O b'     C-down
-  ble-bind -k 'ESC [ b'     S-down
-  ble-bind -k 'ESC O c'     C-right
-  ble-bind -k 'ESC [ c'     S-right
-  ble-bind -k 'ESC O d'     C-left
-  ble-bind -k 'ESC [ d'     S-left
-  ble-bind -k 'ESC [ 2 $'   S-insert # ECMA-48 違反
-  ble-bind -k 'ESC [ 3 $'   S-delete # ECMA-48 違反
-  ble-bind -k 'ESC [ 5 $'   S-prior  # ECMA-48 違反
-  ble-bind -k 'ESC [ 6 $'   S-next   # ECMA-48 違反
-  ble-bind -k 'ESC [ 7 $'   S-home   # ECMA-48 違反
-  ble-bind -k 'ESC [ 8 $'   S-end    # ECMA-48 違反
-  ble-bind -k 'ESC [ 2 3 $' S-f11    # ECMA-48 違反
-  ble-bind -k 'ESC [ 2 4 $' S-f12    # ECMA-48 違反
-  ble-bind -k 'ESC [ 2 5 $' S-f13    # ECMA-48 違反
-  ble-bind -k 'ESC [ 2 6 $' S-f14    # ECMA-48 違反
-  ble-bind -k 'ESC [ 2 8 $' S-f15    # ECMA-48 違反
-  ble-bind -k 'ESC [ 2 9 $' S-f16    # ECMA-48 違反
-  ble-bind -k 'ESC [ 3 1 $' S-f17    # ECMA-48 違反
-  ble-bind -k 'ESC [ 3 2 $' S-f18    # ECMA-48 違反
-  ble-bind -k 'ESC [ 3 3 $' S-f19    # ECMA-48 違反
-  ble-bind -k 'ESC [ 3 4 $' S-f20    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi 'Z'     S-TAB
+  ble/init:cmap/bind-single-ss3 'a'     C-up
+  ble/init:cmap/bind-single-csi 'a'     S-up
+  ble/init:cmap/bind-single-ss3 'b'     C-down
+  ble/init:cmap/bind-single-csi 'b'     S-down
+  ble/init:cmap/bind-single-ss3 'c'     C-right
+  ble/init:cmap/bind-single-csi 'c'     S-right
+  ble/init:cmap/bind-single-ss3 'd'     C-left
+  ble/init:cmap/bind-single-csi 'd'     S-left
+  ble/init:cmap/bind-single-csi '2 $'   S-insert # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '3 $'   S-delete # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '5 $'   S-prior  # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '6 $'   S-next   # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '7 $'   S-home   # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '8 $'   S-end    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '2 3 $' S-f11    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '2 4 $' S-f12    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '2 5 $' S-f13    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '2 6 $' S-f14    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '2 8 $' S-f15    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '2 9 $' S-f16    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '3 1 $' S-f17    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '3 2 $' S-f18    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '3 3 $' S-f19    # ECMA-48 違反
+  ble/init:cmap/bind-single-csi '3 4 $' S-f20    # ECMA-48 違反
 
   # cygwin specific
-  ble-bind -k 'ESC [ [ A' f1
-  ble-bind -k 'ESC [ [ B' f2
-  ble-bind -k 'ESC [ [ C' f3
-  ble-bind -k 'ESC [ [ D' f4
-  ble-bind -k 'ESC [ [ E' f5
+  ble/init:cmap/bind-single-csi '[ A' f1
+  ble/init:cmap/bind-single-csi '[ B' f2
+  ble/init:cmap/bind-single-csi '[ C' f3
+  ble/init:cmap/bind-single-csi '[ D' f4
+  ble/init:cmap/bind-single-csi '[ E' f5
 
   # 修飾キー 'CAN @ ?'
   #
