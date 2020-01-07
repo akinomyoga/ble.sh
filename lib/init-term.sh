161af07a92a712ebe3fa908fd9a9c9d9c470f3da
Koichi Murase <myoga.murase@gmail.com>
2020-01-07 13:21:28 +0800

    term: support "tput" based on termcap entries

--- lib/init-term.sh
+++ lib/init-term.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+function ble/init:term/tput { return 1; }
 if ble/bin/.freeze-utility-path tput; then
   _ble_term_hasput=1
-  function ble/init:term/tput { ble/bin/tput "$@" 2>/dev/null; }
-else
-  function ble/init:term/tput { return 1; }
+  if ble/bin/tput cuu 1 &>/dev/null; then
+    function ble/init:term/tput { ble/bin/tput "${1%%:*}" "${@:2}" 2>/dev/null; }
+  elif ble/bin/tput UP 1 &>/dev/null; then
+    function ble/init:term/tput { ble/bin/tput "${1#*:}" "${@:2}" 2>/dev/null; }
+  fi
 fi
 
 function ble/init:term/register-varname {
@@ -57,21 +60,21 @@ function ble/init:term/initialize {
   # xenl (end of line behavior)
   _ble_term_xenl=1
   [[ $_ble_term_hasput ]] &&
-    ! ble/init:term/tput xenl &>/dev/null &&
+    ! ble/init:term/tput xenl:xn &>/dev/null &&
     _ble_term_xenl=0
   ble/init:term/register-varname _ble_term_xenl
 
   # tab width
   _ble_term_it=8
   if [[ $_ble_term_hasput ]]; then
-    ble/util/assign _ble_term_it 'ble/init:term/tput it'
+    ble/util/assign _ble_term_it 'ble/init:term/tput it:it'
     _ble_term_it=${_ble_term_it:-8}
   fi
   ble/init:term/register-varname _ble_term_it
 
   # IND/RI, CR, LF, FS
-  ble/init:term/define-cap.2 _ble_term_ind $'\eD' ind
-  ble/init:term/define-cap   _ble_term_ri  $'\eM' ri
+  ble/init:term/define-cap.2 _ble_term_ind $'\eD' ind:sf
+  ble/init:term/define-cap   _ble_term_ri  $'\eM' ri:sr
   ble/init:term/define-cap   _ble_term_cr  $'\r'  cr
   _ble_term_nl=$'\n'
   ble/init:term/register-varname _ble_term_nl
@@ -81,10 +84,10 @@ function ble/init:term/initialize {
   ble/init:term/register-varname _ble_term_fs
 
   # CUU/CUD/CUF/CUB
-  ble/init:term/define-cap _ble_term_cuu $'\e[%dA' cuu 123
-  ble/init:term/define-cap _ble_term_cud $'\e[%dB' cud 123
-  ble/init:term/define-cap _ble_term_cuf $'\e[%dC' cuf 123
-  ble/init:term/define-cap _ble_term_cub $'\e[%dD' cub 123
+  ble/init:term/define-cap _ble_term_cuu $'\e[%dA' cuu:UP 123
+  ble/init:term/define-cap _ble_term_cud $'\e[%dB' cud:DO 123
+  ble/init:term/define-cap _ble_term_cuf $'\e[%dC' cuf:RI 123
+  ble/init:term/define-cap _ble_term_cub $'\e[%dD' cub:LE 123
   _ble_term_cuu=${_ble_term_cuu//123/%d}
   _ble_term_cud=${_ble_term_cud//123/%d}
   _ble_term_cuf=${_ble_term_cuf//123/%d}
@@ -92,32 +95,32 @@ function ble/init:term/initialize {
   # ※もし 122 だとか 124 だとかになると上記では駄目
 
   # CUP
-  ble/init:term/define-cap _ble_term_cup $'\e[13;35H' cup 12 34
+  ble/init:term/define-cap _ble_term_cup $'\e[13;35H' cup:cm 12 34
   _ble_term_cup=${_ble_term_cup//13/%l}
   _ble_term_cup=${_ble_term_cup//35/%c}
   _ble_term_cup=${_ble_term_cup//12/%y}
   _ble_term_cup=${_ble_term_cup//34/%x}
 
   # CHA HPA VPA
-  ble/init:term/define-cap _ble_term_hpa "$_ble_term_cr${_ble_term_cuf//'%d'/123}" hpa 123
+  ble/init:term/define-cap _ble_term_hpa "$_ble_term_cr${_ble_term_cuf//'%d'/123}" hpa:ch 123
   _ble_term_hpa=${_ble_term_hpa//123/%x}
   _ble_term_hpa=${_ble_term_hpa//124/%c}
-  ble/init:term/define-cap _ble_term_vpa "${_ble_term_cuu//'%d'/199}${_ble_term_cud//'%d'/123}" vpa 123
+  ble/init:term/define-cap _ble_term_vpa "${_ble_term_cuu//'%d'/199}${_ble_term_cud//'%d'/123}" vpa:cv 123
   _ble_term_vpa=${_ble_term_vpa//123/%y}
   _ble_term_vpa=${_ble_term_vpa//124/%l}
 
   # CUP+ED (clear_screen)
-  ble/init:term/define-cap _ble_term_clear $'\e[H\e[2J' clear
+  ble/init:term/define-cap _ble_term_clear $'\e[H\e[2J' clear:cl
 
   # IL/DL
-  ble/init:term/define-cap _ble_term_il $'\e[%dL' il 123
-  ble/init:term/define-cap _ble_term_dl $'\e[%dM' dl 123
+  ble/init:term/define-cap _ble_term_il $'\e[%dL' il:AL 123
+  ble/init:term/define-cap _ble_term_dl $'\e[%dM' dl:DL 123
   _ble_term_il=${_ble_term_il//123/%d}
   _ble_term_dl=${_ble_term_dl//123/%d}
 
   # EL
-  ble/init:term/define-cap _ble_term_el  $'\e[K'  el
-  ble/init:term/define-cap _ble_term_el1 $'\e[1K' el1
+  ble/init:term/define-cap _ble_term_el  $'\e[K'  el:ce
+  ble/init:term/define-cap _ble_term_el1 $'\e[1K' el1:cb
   if [[ $_ble_term_el == $'\e[K' && $_ble_term_el1 == $'\e[1K' ]]; then
     _ble_term_el2=$'\e[2K'
   else
@@ -126,26 +129,26 @@ function ble/init:term/initialize {
   ble/init:term/register-varname _ble_term_el2
 
   # ED
-  ble/init:term/define-cap _ble_term_ed  $'\e[J' ed
+  ble/init:term/define-cap _ble_term_ed  $'\e[J' ed:cd
 
   # ICH/DCH/ECH
   #   Note: 必ずしも対応しているか分からないので terminfo に載っている時のみ使う。
-  ble/init:term/define-cap _ble_term_ich '' ich 123 # CSI @
-  ble/init:term/define-cap _ble_term_dch '' dch 123 # CSI P
-  ble/init:term/define-cap _ble_term_ech '' ech 123 # CSI X
+  ble/init:term/define-cap _ble_term_ich '' ich:IC 123 # CSI @
+  ble/init:term/define-cap _ble_term_dch '' dch:DC 123 # CSI P
+  ble/init:term/define-cap _ble_term_ech '' ech:ec 123 # CSI X
   _ble_term_ich=${_ble_term_ich//123/%d}
   _ble_term_dch=${_ble_term_dch//123/%d}
   _ble_term_ech=${_ble_term_ech//123/%d}
 
   # DECSC/DECRC or SCOSC/SCORC
-  ble/init:term/define-cap _ble_term_sc $'\e[s' sc
-  ble/init:term/define-cap _ble_term_rc $'\e[u' rc
+  ble/init:term/define-cap _ble_term_sc $'\e[s' sc:sc
+  ble/init:term/define-cap _ble_term_rc $'\e[u' rc:rc
 
   # Cursors
-  ble/init:term/define-cap _ble_term_Ss '' ss 123 # DECSCUSR
+  ble/init:term/define-cap _ble_term_Ss '' ss:- 123 # DECSCUSR
   _ble_term_Ss=${_ble_term_Ss//123/@1}
-  ble/init:term/define-cap _ble_term_cvvis $'\e[?25h' cvvis
-  ble/init:term/define-cap _ble_term_civis $'\e[?25l' civis
+  ble/init:term/define-cap _ble_term_cvvis $'\e[?25h' cvvis:vs
+  ble/init:term/define-cap _ble_term_civis $'\e[?25l' civis:vi
   # xterm の terminfo が点滅まで勝手に変更するので消す。
   [[ $_ble_term_cvvis == $'\e[?12;25h' || $_ble_term_cvvis == $'\e[?25;12h' ]] &&
     _ble_term_cvvis=$'\e[?25h'
@@ -156,23 +159,23 @@ function ble/init:term/initialize {
     _ble_term_cvvis=$_ble_term_cvvis$'\e[?25h'
 
   # SGR clear
-  ble/init:term/define-cap _ble_term_sgr0 $'\e[m' sgr0
+  ble/init:term/define-cap _ble_term_sgr0 $'\e[m' sgr0:me
 
   # SGR misc
-  ble/init:term/define-cap _ble_term_bold  $'\e[1m' bold
-  ble/init:term/define-cap _ble_term_blink $'\e[5m' blink
-  ble/init:term/define-cap _ble_term_rev   $'\e[7m' rev
-  ble/init:term/define-cap _ble_term_invis $'\e[8m' invis
+  ble/init:term/define-cap _ble_term_bold  $'\e[1m' bold:md
+  ble/init:term/define-cap _ble_term_blink $'\e[5m' blink:mb
+  ble/init:term/define-cap _ble_term_rev   $'\e[7m' rev:mr
+  ble/init:term/define-cap _ble_term_invis $'\e[8m' invis:mk
   ble/init:term/define-sgr-param _ble_term_sgr_bold  "$_ble_term_bold"  1
   ble/init:term/define-sgr-param _ble_term_sgr_blink "$_ble_term_blink" 5
   ble/init:term/define-sgr-param _ble_term_sgr_rev   "$_ble_term_rev"   7
   ble/init:term/define-sgr-param _ble_term_sgr_invis "$_ble_term_invis" 8
-  ble/init:term/define-cap _ble_term_sitm $'\e[3m'  sitm
-  ble/init:term/define-cap _ble_term_ritm $'\e[23m' ritm
-  ble/init:term/define-cap _ble_term_smul $'\e[4m'  smul
-  ble/init:term/define-cap _ble_term_rmul $'\e[24m' rmul
-  ble/init:term/define-cap _ble_term_smso $'\e[7m'  smso
-  ble/init:term/define-cap _ble_term_rmso $'\e[27m' rmso
+  ble/init:term/define-cap _ble_term_sitm $'\e[3m'  sitm:ZH
+  ble/init:term/define-cap _ble_term_ritm $'\e[23m' ritm:ZR
+  ble/init:term/define-cap _ble_term_smul $'\e[4m'  smul:us
+  ble/init:term/define-cap _ble_term_rmul $'\e[24m' rmul:ue
+  ble/init:term/define-cap _ble_term_smso $'\e[7m'  smso:so
+  ble/init:term/define-cap _ble_term_rmso $'\e[27m' rmso:se
   ble/init:term/define-sgr-param _ble_term_sgr_sitm "$_ble_term_sitm" 3
   ble/init:term/define-sgr-param _ble_term_sgr_ritm "$_ble_term_ritm" 23
   ble/init:term/define-sgr-param _ble_term_sgr_smul "$_ble_term_smul" 4
@@ -189,7 +192,7 @@ function ble/init:term/initialize {
   fi
 
   # SGR colors
-  ble/init:term/define-cap _ble_term_colors 256 colors
+  ble/init:term/define-cap _ble_term_colors 256 colors:Co
   local i
   _ble_term_setaf=()
   _ble_term_setab=()
@@ -207,11 +210,11 @@ function ble/init:term/initialize {
               (i1==4?1:i1))))))
       local j=$((i-i1+j1))
 
-      ble/util/assign af 'ble/init:term/tput setaf "$i" 2>/dev/null'
-      [[ $af ]] || ble/util/assign af 'ble/init:term/tput setf "$j" 2>/dev/null'
+      ble/util/assign af 'ble/init:term/tput setaf:AF "$i" 2>/dev/null'
+      [[ $af ]] || ble/util/assign af 'ble/init:term/tput setf:Sf "$j" 2>/dev/null'
 
-      ble/util/assign ab 'ble/init:term/tput setab "$i" 2>/dev/null'
-      [[ $ab ]] || ble/util/assign ab 'ble/init:term/tput setb "$j" 2>/dev/null'
+      ble/util/assign ab 'ble/init:term/tput setab:AB "$i" 2>/dev/null'
+      [[ $ab ]] || ble/util/assign ab 'ble/init:term/tput setb:Sb "$j" 2>/dev/null'
     fi
 
     # default value
