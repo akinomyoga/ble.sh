81298166c04e7cab1972dee529cf56a76d317c6c
Koichi Murase <myoga.murase@gmail.com>
2020-01-24 22:09:31 +0800

    fixup ab01ceb

--- term.sh
+++ term.sh
@@ -18,13 +18,13 @@ function ble/term.sh/register-varname {
 function ble/term.sh/define-cap {
   local name="$1" def="$2"
   shift 2
-  builtin eval "$name=\"\$(ble/term.sh/tput \"\$@\" || echo -n \"\$def\")\""
+  builtin eval "$name=\"\$(ble/term.sh/tput $* || echo -n \"\$def\")\""
   ble/term.sh/register-varname "$name"
 }
 function ble/term.sh/define-cap.2 {
   local name="$1" def="$2"
   shift 2
-  builtin eval "$name=\"\$(echo -n x;ble/term.sh/tput \"\$@\" || echo -n \"\$def\";echo -n x)\"; $name=\${$name#x}; $name=\${$name%x}"
+  builtin eval "$name=\"\$(echo -n x;ble/term.sh/tput $* || echo -n \"\$def\";echo -n x)\"; $name=\${$name#x}; $name=\${$name%x}"
   ble/term.sh/register-varname "$name"
 }
 
