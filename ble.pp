4ef844ee5646e51d61b581bfff2fa86e4c6baffd
Koichi Murase <myoga.murase@gmail.com>
2021-10-25 12:16:13 +0900

    main: suppress non-interactive warnings from manually sourced startup files

--- ble.pp
+++ ble.pp
@@ -45,8 +45,10 @@ case ${BASH_VERSINFO[4]} in
 esac
 
 if [[ $- != *i* ]]; then
-  { ((${#BASH_SOURCE[@]})) && case ${BASH_SOURCE[${#BASH_SOURCE[@]}-1]} in (*bashrc|*bash_profile) ;; (*) false ;; esac } ||
-    echo "ble.sh: This is not an interactive session." >&2
+  case " ${BASH_SOURCE[*]##*/} " in
+  (*' .bashrc '* | *' .bash_profile '* | *' .profile '* | *' bashrc '* | *' profile '*) false ;;
+  esac  &&
+    builtin echo "ble.sh: This is not an interactive session." >&2 || ((1))
   return 1 2>/dev/null || exit 1
 fi
 
